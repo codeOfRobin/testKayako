e3b81f86cc6532a55b297ceea9d7df77029b38d3
Robin Malhotra <malhotra.robin@yahoo.co.in>
2017-10-09 21:53:08 +0530

    Fix for crash

--- testKayako/AppDelegate.m
+++ testKayako/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <AsyncDisplayKit/AsyncDisplayKit.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+	[ASControlNode setEnableHitTestDebug:YES];
     return YES;
 }
 
